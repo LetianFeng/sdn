c5c0dd479da9b8560ea086b685bdacd71788fb54
Letian Feng <letian.feng@hotmail.com>
2016-11-10 21:55:42 +0100

    first commit containing lab sol

---
+++ lab1/sdn-lab1-rules-name.sh
@@ -0,0 +1,14 @@
+# This is the submission from below students:
+# Zhen Chen(2665935), Chunyuan Yu(2587628), Letian Feng(2255840)
+
+# Unicast
+dpctl add-flow tcp:127.0.0.1:6634 dl_dst=00:00:00:00:00:01,idle_timeout=0,actions=1
+dpctl add-flow tcp:127.0.0.1:6634 dl_dst=00:00:00:00:00:02,idle_timeout=0,actions=2
+dpctl add-flow tcp:127.0.0.1:6634 dl_dst=00:00:00:00:00:03,idle_timeout=0,actions=3
+
+# Broadcast
+dpctl add-flow tcp:127.0.0.1:6634 dl_dst=FF:FF:FF:FF:FF:FF,idle_timeout=0,actions=flood
+
+# Spam Filter
+dpctl add-flow tcp:127.0.0.1:6634 tp_src=25,idle_timeout=0,actions=
+
