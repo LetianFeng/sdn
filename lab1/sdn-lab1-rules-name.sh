7770f44bfa6c1ab046fd239775091441a73c94f4
Letian Feng <letian.feng@hotmail.com>
2016-11-13 23:09:42 +0100

    priority for spam filter updated

--- lab1/sdn-lab1-rules-name.sh
+++ lab1/sdn-lab1-rules-name.sh
@@ -10,5 +10,4 @@ dpctl add-flow tcp:127.0.0.1:6634 dl_dst=00:00:00:00:00:03,idle_timeout=0,action
 dpctl add-flow tcp:127.0.0.1:6634 dl_dst=FF:FF:FF:FF:FF:FF,idle_timeout=0,actions=flood
 
 # Spam Filter
-dpctl add-flow tcp:127.0.0.1:6634 tp_dst=25,idle_timeout=0,actions=
-
+dpctl add-flow tcp:127.0.0.1:6634 tp_dst=25,idle_timeout=0,priority=33000,actions=
